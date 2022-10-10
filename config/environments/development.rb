7b6bf0f12efcce4d6dd3a9d62497105448133793
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-10-10 14:06:36 +0000

    rails g controller homes topでhomesのcontroller, view, routingを作成。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "b06935af522e4bc99e64a5d860b1d094.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  # config.hosts << "b06935af522e4bc99e64a5d860b1d094.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts.clear
 end
