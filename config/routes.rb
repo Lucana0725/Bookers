f5b6f56b2746261234dcb93e61d3f07d2e0d7171
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-10-11 13:50:35 +0000

    [Finish]topページのルーティング、bookのindexページへのリンクの作成、indexページの表示まで。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   get 'books/new'
-  get 'books/index'
+  # get 'books/index'
+  get 'books' => 'books#index'
   get 'books/show'
   get 'books/edit'
-  get 'homes/top'
+  get '/' => 'homes#top'
+  
+  # 【補足】getの後の''にはURLを、=>後の''にはアクション名が入る
+  # get 'homes/top'
+  # get 'top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
