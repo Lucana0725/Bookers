85b14a1e34460ebd70dbcf9d576b0f09d6d9f0fe
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-10-12 14:09:20 +0000

    [Update]migrationファイルの変更(titleとbodyカラムを追加できていなかった)、indexに表示はされていないがSQLの動作は確認。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,15 @@
 class BooksController < ApplicationController
   def new
+    # Bookモデルをもとにオブジェクトを作成、インスタンス変数として保持してviewで用いる
+    # (= Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する)
     @book = Book.new
+  end
+  
+  # createアクションの作成
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books' # showを作ってないので一旦indexへ変遷させる
   end
 
   def index
@@ -10,5 +19,12 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+  
+
+  private
+  # ストロングパラメーター
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
