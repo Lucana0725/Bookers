9950db9be888d93fc5348a02f8e848aaa5f1703a
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-10-14 09:51:07 +0000

    [Update]新規投稿後、indexページへ遷移していたのをshowページへ遷移するよう変更。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,8 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books' # showを作ってないので一旦indexへ変遷させる
+    # redirect_to '/books'  showを作ってないので一旦indexへ遷移させる
+    redirect_to book_path(book.id)  # showを作ったのでshowへ。
   end
 
   def index
