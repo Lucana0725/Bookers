27ee6a13ba06ec9f03698b7fdde1f59938b7f9ee
EC2 Default User <ec2-user@ip-172-31-41-66.ap-northeast-1.compute.internal>
2022-10-15 14:25:38 +0000

    [Finish]RSpecのインストールの完了、第一回テスト実施。これから修正作業に。

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 2022_10_12_135711) do
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
     t.string "title"
     t.string "body"
   end
